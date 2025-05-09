f037750424d4a60114f0c35fba2612ce867b5c13
Shubham Kumar <malhotrashubham48@gmail.com>
2025-05-10 02:10:03 +0530

    cors-fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" 
+    ? "http://localhost:5001/api" 
+    : "http://34.9.239.34:5001/api",
   withCredentials: true,
 });
